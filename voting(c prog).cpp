f3f8c3cad80f85bb825a45f31c00d5b13f1b4aca
D Bamini <112744608+baminid@users.noreply.github.com>
2022-09-21 14:25:53 +0530

    initiate commit

---
+++ voting(c prog).cpp
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+int main()
+{
+	int a ;
+	
+	
+	printf("Enter the age of the person: ");
+	scanf("%d",&a);
+
+	
+	if (a>=18)
+	{
+		printf("Eligible for voting");
+	}
+	else
+	{
+		printf("Not eligible for voting\n");
+	}	
+
+	return 0;
+}
